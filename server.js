2a05cd8b4b185961bd587ddb73cecf88d9d83dff
Pierre Trochu <pierre@Pierres-MacBook-Pro.local>
2018-03-31 17:52:59 +0100

    trying to see the folder

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ var app = express(); //create app
 var path = require('path'); //built in path module, used to resolve paths of relative files
 var port = 8000 //stores port number to listen on
 
+console.log(path.join(__dirname, 'Annotationsample.html'));
 app.use(express.static(path.join(__dirname + '/css'))); //allows html file to reference stylesheet that is stored in ./css directory
 app.use(express.static(path.join(__dirname + '/js'))); //allows html file to reference js
 
 app.get('/', function(req, res) { //on html request of root directory, run callback function
-    res.sendFile(__dirname + 'Annotationsample.html'); //send html file named
+    res.sendFile(path.join(__dirname, 'Annotationsample.html')); //send html file named
 });
 
 app.listen(port);//listen for network traffic on port specified by port variable
