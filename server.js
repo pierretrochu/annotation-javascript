f1d5445b0a7c16ad13ef24dc96a541bbc490cdc5
Pierre Trochu <pierre@Pierres-MacBook-Pro.local>
2018-03-31 18:30:05 +0100

    Serving the right port

--- server.js
+++ server.js
@@ -8,16 +8,11 @@ console.log(path.join(__dirname, 'Annotationsample.html'));
 app.use(express.static(path.join(__dirname + '/css'))); //allows html file to reference stylesheet that is stored in ./css directory
 app.use(express.static(path.join(__dirname + '/js'))); //allows html file to reference js
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+app.get('/', function(req, res) { //on html request of root directory, run callback function
+    res.sendFile(path.join(__dirname, 'Annotationsample.html')); //send html file named
+});
+
 
 app.listen(port);//listen for network traffic on port specified by port variable
 
 console.log("Now listening on port " + port); //write to the console which port is being used
-
-/*
-function(req, res) { //on html request of root directory, run callback function
-    res.sendFile(path.join(__dirname, 'Annotationsample.html')); //send html file named
-});
-*/
