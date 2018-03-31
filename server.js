41260df9ed94523880707f66ad7bce6c6044b8e4
Pierre Trochu <pierre@Pierres-MacBook-Pro.local>
2018-03-31 16:23:26 +0100

    trying simpler version of server

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 var http = require('http');
 var fs = require('fs');
 
-/*
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
 
-*/
 
+/*
 function send404Response(response) {
   response.writeHead(404, {"Content-Type": "text/plain"});
   response.write('Error 404 - sorry the page was not found!');
@@ -28,3 +27,4 @@ function onRequest(request, response) {
 }
 
 http.createServer(onRequest).listen(8080);
+*/
