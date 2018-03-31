39a79332bfd213128c356b3152b01dd3d02ac168
Pierre Trochu <pierre@Pierres-MacBook-Pro.local>
2018-03-31 18:54:29 +0100

    test folder

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express(); //create app
 var path = require('path'); //built in path module, used to resolve paths of relative files
 var port = 8080 //stores port number to listen on
 
-console.log(path.join(__dirname, 'Annotationsample.html'));
+app.use(express.static(path.join(__dirname + '/testers'))); //allows html file to reference stylesheet that is stored in ./css directory
 app.use(express.static(path.join(__dirname + '/css'))); //allows html file to reference stylesheet that is stored in ./css directory
 app.use(express.static(path.join(__dirname + '/js'))); //allows html file to reference js
 
