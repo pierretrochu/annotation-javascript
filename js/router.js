491f7faacad69e2bee7e3df46ef715b28036d13f
Pierre Trochu <pierre@Pierres-MacBook-Pro.local>
2018-04-01 16:31:21 +0100

    Separating router as a module

---
+++ js/router.js
@@ -0,0 +1,13 @@
+var express = require ('express');
+var router = express.Router();
+var path = require('path'); //built in path module, used to resolve paths of relative files
+
+router.get('/', function (req, res) {
+  res.sendFile('/Annotationsample.html', { root: "./"}); //send html file named
+  })
+
+router.get('/testers', function (req, res) {
+    res.sendFile('/file.html', { root: "./testers"}); //send html file named
+    })
+
+module.exports = router;
